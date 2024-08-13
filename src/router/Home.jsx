4e0b6017a58a3cd17f389483db53cf06bfa7f5ae
chuans <juaazn.05@gmail.com>
2024-08-13 14:51:29 +0200

    Component Home

--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -1,33 +1,12 @@
-import { useState, useEffect } from 'react'
+import Layout from '../layout/Layout'
+import Card from '../components/Card'
+import Banner from '../components/Banner'
 
 export default function () {
-  const [article, setArticle] = useState([])
-
-  const apiArticle = async () => {
-    try {
-      const response = await fetch('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=aqbSZcqI1bEGnQg8fZstWrTZtnME9L61')
-      const data = await response.json()
-      setArticle(data.results)
-    } catch(err) {
-      console.error(err)
-    }
-  }
-
-  useEffect(() => {
-    apiArticle()
-  }, [])
-
   return (
-    <>
-      <h1>Hola</h1>
-      {
-        article.map(article => (
-          <div key={article.uri}>
-            <h2>{article.title}</h2>
-            <p>{article.abstract}</p>
-          </div>
-        ))
-      }
-    </>
+    <Layout>
+      <Banner />
+      <Card />
+    </Layout> 
   )
 }
