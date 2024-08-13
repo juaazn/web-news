b3ec08f2851f6dce2bab75ea4b1011dee206a335
chuans <juaazn.05@gmail.com>
2024-08-13 14:51:04 +0200

    Component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import Logo from './Logo'
+import styles from '../styles/Header/header.module.css'
+import Buttom from './Buttom'
+import { Link } from 'react-router-dom'
+
 export default function () {
   return (
-    <>
-      <p>Header</p>
-    </>
+    <header className={styles.container}>
+      <Logo />
+      <div className={styles.container_link}>
+        <ul className={styles.flex_link}>
+          <li><Link to='/'>Noticias</Link></li>
+          <li><Link to='/list'>Lista</Link></li>
+        </ul>
+        <Buttom name='Únete'/>
+      </div>
+    </header>
   )
 }
