f3eabe816c65a880dd5375501eab5cac8f852870
chuans <juaazn.05@gmail.com>
2024-08-15 14:53:49 +0200

    Insert component form

--- src/router/Form.jsx
+++ src/router/Form.jsx
@@ -1,9 +1,14 @@
 import Layout from '../layout/Layout'
+import styles  from '../styles/form/from.module.css'
+import Form from '../components/Form'
+
 
 export default function () {
   return (
     <Layout>
-      
+      <main className={styles.contiener}>
+        <Form />
+      </main>
     </Layout>
   )
 }
