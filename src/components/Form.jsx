c6f3e68ce4e5756475d2a541884ba93e08c2d815
chuans <juaazn.05@gmail.com>
2024-08-15 14:53:39 +0200

    Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,30 @@
+import { useState  } from 'react'
+
 export default function () {
+  const initialState = { username: '', email: '' }
+  const [data, setData] = useState({ username: '', email: '' })
+
+   const handleInput = (event) => {
+      setData({ ...data, [event.target.name]: event.target.value })
+   }
+
+  const clearState = () => {
+    setData({ ...initialState })
+  }
+
+  const handleSubmit  = (event) => {
+    event.preventDefault()
+    window.localStorage.setItem('user', JSON.stringify(data))
+    clearState()
+  }
+
   return (
-    <>
-      <p>Form</p>
-    </>
+    <form onSubmit={handleSubmit }>
+      <label htmlFor="username">Name: </label>
+      <input id="username" type="text" name='username' onChange={handleInput}/>
+      <label htmlFor="email">Email: </label>
+      <input id="email" type="email" name='email' onChange={handleInput} />
+      <input type="submit" />
+    </form>
   )
 }
