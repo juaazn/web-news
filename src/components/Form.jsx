1bded4d8855056431c875fe02872cf0ad85012fe
chuans <juaazn.05@gmail.com>
2024-08-15 21:58:34 +0200

    Component form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState  } from 'react'
+import styles from '../styles/form/from.module.css'
 
 export default function () {
   const initialState = { username: '', email: '' }
@@ -19,12 +20,16 @@ export default function () {
   }
 
   return (
-    <form onSubmit={handleSubmit }>
-      <label htmlFor="username">Name: </label>
-      <input id="username" type="text" name='username' onChange={handleInput}/>
-      <label htmlFor="email">Email: </label>
-      <input id="email" type="email" name='email' onChange={handleInput} />
-      <input type="submit" />
-    </form>
+    <section className={styles.container_form}>
+      <div className={styles.content}>
+        <h1>Subscribe to more newsletters</h1>
+        <p>Subscribe to access the latest news quickly and easily with just one click.</p>
+      </div>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="text" name='username' onChange={handleInput} placeholder='Name'/>
+        <input id="email" type="email" name='email' onChange={handleInput} placeholder='Email'/>
+        <input type="submit" />
+      </form>
+    </section>
   )
 }
