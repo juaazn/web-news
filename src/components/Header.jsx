368f3bdb4ff47ae00a79b77964815edc86aad67d
chuans <juaazn.05@gmail.com>
2024-08-16 16:45:03 +0200

    added React Conditional Rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,30 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { subcribeGlobalContext } from '../context/subcribeContext'
 import Logo from './Logo'
 import styles from '../styles/Header/header.module.css'
 import Buttom from './Buttom'
 
 export default function () {
+  const { subcribe } = useContext(subcribeGlobalContext)
+
   return (
     <header className={styles.container}>
       <Logo />
       <div className={styles.container_link}>
         <ul className={styles.flex_link}>
           <li><Link to='/'>Home</Link></li>
-          <li><Link to='/list'>Noticias</Link></li>
+          { 
+            subcribe === true ?
+              <li><Link to='/list'>News</Link></li>
+              : <p className={styles.disabled_button}>News</p>
+          }
         </ul>
-        <Buttom name='Únete'/>
+        {
+          subcribe === false ?
+            <Buttom name='Join us'/>
+            : null
+        }
       </div>
     </header>
   )
