90a6f8bfc5ff83db4d830860ddf5da40261e93de
chuans <juaazn.05@gmail.com>
2024-08-16 16:45:58 +0200

    update context

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import { useContext } from 'react'
+import { subcribeGlobalContext } from '../context/subcribeContext'
+import { useNavigate } from 'react-router-dom'
 
 export default function useData () {
   const initialState = { username: '', email: '' }
   const [data, setData] = useState({ username: '', email: '' })
+  const { setSubcribe } = useContext(subcribeGlobalContext)
 
+  const navigate = useNavigate()
+  
   const handleInput = (event) => {
     setData({ ...data, [event.target.name]: event.target.value })
   }
@@ -12,10 +18,15 @@ export default function useData () {
     setData({ ...initialState })
   }
 
+
+  const checkSubcribe = () => setSubcribe(true)
+
   const handleSubmit  = (event) => {
     event.preventDefault()
     window.localStorage.setItem(`${data.username}`, JSON.stringify(data))
     clearState()
+    navigate('/list')
+    checkSubcribe()
   }
 
   return {
