ae7bb1de06132e1cca6197d1efbe840143ac7506
chuans <juaazn.05@gmail.com>
2024-08-17 00:23:32 +0200

    Component Article

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
-export default function () {
+import styles from '../styles/footer/footer.module.css'
+
+export default function Footer () {
   return (
-    <>
-      <p>Footer</p>
-    </>
+    <footer className={styles.container}>
+      <div className={styles.content}>
+        <img src="/logo.svg" alt="Logo" />
+        <h4>Newspaper</h4>
+      </div>
+      <div>
+        <p><strong>Design inspired by: </strong> <q><a href="https://dribbble.com/shots/6218707-Branding-Agency-News-Page" target="_blank" rel="noopener noreferrer">Branding Agency - News Page</a></q> <cite>Broklin Onjei</cite></p>
+      </div>
+    </footer>
   )
 }
